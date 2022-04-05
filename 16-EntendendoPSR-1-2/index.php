00d3ed2edc3d36848d90462180614c4e573d8609
Anderson <99144364+AndersonMGarcia@users.noreply.github.com>
2022-04-05 07:53:03 -0300

    PSR

---
+++ 16-EntendendoPSR-1-2/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>O que é PSR</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div>
+        <a href="https://www.php-fig.org/psr/psr-1/">PSR-1: Basic Coding Standard</a>
+        <?php
+              
+        ?>
+    </div>   
+</body>
+</html>
